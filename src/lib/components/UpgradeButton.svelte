a9fe1b9379f14a5a47babc73c21f2d89a40d7e27
cute_catgirl <benpmoon@gmail.com>
2024-11-26 23:11:19 -0700

    add tooltips

--- src/lib/components/UpgradeButton.svelte
+++ src/lib/components/UpgradeButton.svelte
@@ -1,18 +1,63 @@
 <script>
-    let { upgradeKey, upgrade, game, calculateCost, applyUpgrade, calculateEffectiveLimit } = $props();
+	import { onMount } from 'svelte';
+	import { offset, flip, shift } from 'svelte-floating-ui/dom';
+	import { createFloatingActions } from 'svelte-floating-ui';
+	import { writable } from 'svelte/store';
+	import { arrow } from 'svelte-floating-ui';
 
-    let count = $derived(game.upgradesOwned[upgradeKey] || 0);
+	let { upgradeKey, upgrade, game, calculateCost, applyUpgrade, calculateEffectiveLimit } =
+		$props();
 
-    let limit = $derived(calculateEffectiveLimit(upgradeKey));
-    let canPurchase = $derived(count < limit);
+	const arrowRef = writable(null);
+	const [floatingRef, floatingContent, update] = createFloatingActions({
+		strategy: 'absolute',
+		placement: 'top',
+		middleware: [arrow({ element: arrowRef })],
+		onComputed({ placement, middlewareData }) {
+			const { x, y } = middlewareData.arrow;
+			const staticSide = {
+				top: 'bottom',
+				right: 'left',
+				bottom: 'top',
+				left: 'right'
+			}[placement.split('-')[0]];
+
+			Object.assign($arrowRef.style, {
+				left: x != null ? `${x}px` : '',
+				top: y != null ? `${y}px` : '',
+				[staticSide]: '-4px'
+			});
+		}
+	});
+
+	let count = $derived(game.upgradesOwned[upgradeKey] || 0);
+
+	let limit = $derived(calculateEffectiveLimit(upgradeKey));
+	let canPurchase = $derived(count < limit);
+
+	let showTooltip = $state(false);
 </script>
 
-<button class="flex flex-col items-center border-slate-900 border-2 rounded p-2 shadow-md active:shadow-inner active:translate-y-0.5 transition-all duration-150" onclick={() => applyUpgrade(upgradeKey)} disabled={!canPurchase}>
-    <div class="font-bold">{upgrade.name}</div>
-    {#if canPurchase}
-        <div class="font-medium">{Math.ceil(calculateCost(upgradeKey))} points</div>
-    {/if}
-    {#if upgrade.limit}
-        <div class="font-normal">{count}/{limit}</div>
-    {/if}
+<button
+	onmouseenter={() => (showTooltip = true)}
+	onmouseleave={() => (showTooltip = false)}
+	class="flex flex-col items-center rounded border-2 border-slate-900 p-2 shadow-md transition-all duration-150 active:translate-y-0.5 active:shadow-inner"
+	onclick={() => applyUpgrade(upgradeKey)}
+	disabled={!canPurchase}
+	use:floatingRef
+>
+	<div class="font-bold">{upgrade.name}</div>
+	{#if canPurchase}
+		<div class="font-medium">{Math.ceil(calculateCost(upgradeKey))} points</div>
+	{/if}
+	{#if upgrade.limit}
+		<div class="font-normal">{count}/{limit}</div>
+	{/if}
 </button>
+
+{#if showTooltip}
+	<div class="absolute rounded bg-slate-200 p-2 shadow-md z-10" use:floatingContent>
+		{upgrade.description}
+		<div class="absolute w-8 h-8 bg-slate-200 rotate-45 -z-10" bind:this={$arrowRef}></div>
+	</div>
+{/if}
